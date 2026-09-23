439d88d7a26857594fc1aec687fbd66eaf3d41ef
Ethan Sherman <ser719577@gmail.com>
2026-09-23 08:22:41 -0300

    finished movie model

---
+++ MoviesAdmin/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; } = string.Empty; // get movie Title
+        public string Syopsis { get; set; }= string.Empty; // get movie description 
+
+        public string Genre { get; set; } = string.Empty; // get movie genre 
+
+        public string Rating {  get; set; } = string.Empty; // get movie rating 
+
+        public int RuntimeMinutes { get; set; } // gets the movies runtime in minutes 
+
+        public DateTime ReleaseDate { get; set; } // gets move date 
+
+    
+
+
+
+    }
+}
